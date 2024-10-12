55baad9447a59d1cac5b7d2cfedfaaec186e3dff
Federico-Taborda <panterx.95@gmail.com>
2024-10-12 11:57:16 -0300

    little changes

--- log_tool.sh
+++ log_tool.sh
@@ -2,6 +2,11 @@
 
 # Guarda el dia actual
 log_date=$(date '+%Y%m%d')
+log_dir="$1"
+
+echo Comprimiendo archivos
 
 # Comprime la carpeta dada con el nombre de la fecha actual
-tar -czf logs.$log_date.tar.gz $1
+tar -czf logs.$log_date.tar.gz $log_dir
+
+echo Archivos comprimidos
